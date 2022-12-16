bc2b75b026124b3b6bed3d3dbeab3e774046e8c4
dima.khoroshev <horoshev.dimaa@gmail.com>
2022-12-16 16:05:43 +0700

    create

---
+++ lab2/libs/CommandState/include/commandstate.h
@@ -0,0 +1,14 @@
+//
+// Created by Дмитрий Хорошев on 16.12.2022.
+//
+
+#ifndef BIGINT_COMMANDSTATE_H
+#define BIGINT_COMMANDSTATE_H
+
+
+class commandstate {
+
+};
+
+
+#endif //BIGINT_COMMANDSTATE_H
---
+++ lab2/libs/CommandState/src/commandstate.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Дмитрий Хорошев on 16.12.2022.
+//
+
+#include "commandstate.h"
