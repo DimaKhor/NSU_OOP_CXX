636f24a3b73266d2b1f74aaaf77cbaf363b73775
dima.khoroshev <horoshev.dimaa@gmail.com>
2022-12-21 00:04:50 +0700

    spelling errors

--- lab2/libs/Message/src/message.cpp
+++ lab2/libs/Message/src/message.cpp
@@ -5,13 +5,13 @@ using namespace std;
 void Message::startMessage() {
     cout << "Hello in my game of life!:)" << endl;
     cout << "There are two mods: offline and online" << endl;
-    cout << "online mode: you just start programm and choose commands" << endl;
+    cout << "online mode: you just start program and choose commands" << endl;
     cout << "offline mode: start it with -file <input file> -i <number of iterations> -o <output file>" << endl;
     cout << "There are such commands in online mode:" << endl;
     cout << "1. help - show game reference." << endl;
-    cout << "2. dump <filename> -save current universe state." << endl;
+    cout << "2. dump <filename> - save current universe state in file." << endl;
     cout << "3. exit - end the game." << endl;
-    cout << "4. tick <n> -count game state after n iteration." << endl;
+    cout << "4. tick <n> - count game state after n iteration." << endl;
     sleep(3);
     system("clear");
 }
